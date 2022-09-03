d785dcf5cc358b398db3d7538145f670f9af2784
parvez islam <parvezislam45@gmail.com>
2022-09-03 15:42:14 +0600

    server code

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,26 +2,26 @@ let users = [
   {
     id: 1,
     gender: "male",
-    name: "Monir",
-    contact: "01925716395",
-    address: "Sherpur",
-    photoUrl: "https://www.facebook.com/moniruzzaman255/",
+    name: "David",
+    contact: "01683338523",
+    address: "Dhaka",
+    photoUrl: "https://scontent.fdac24-1.fna.fbcdn.net/v/t39.30808-6/275715237_4949305155184344_833798656798632055_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGBT9mU6WP2G3G8PQLyoqO6FvIZuWeMO8wW8hm5Z4w7zHoo8uGc8j6m6BtEMMVOtBYBIBBU2gR-J260SLI-1hYR&_nc_ohc=7Pzjszuhu_sAX-TkmDd&_nc_ht=scontent.fdac24-1.fna&oh=00_AT-zO2RDRuVYjknJ7S05MAJr_DpTFX7cohZzDkOdhRar4w&oe=6318FD9B",
   },
   {
     id: 2,
     gender: "male",
-    name: "Aonik",
-    contact: "01925716395",
-    address: "Sherpur",
-    photoUrl: "https://www.facebook.com/moniruzzaman255/",
+    name: "Parvez",
+    contact: "01723726665",
+    address: "Narsingdi",
+    photoUrl: "https://scontent.fdac24-2.fna.fbcdn.net/v/t1.6435-9/154368297_3774950732619798_6682339353723604282_n.jpg?stp=dst-jpg_p843x403&_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGjbILQZ_SVSO0kQYOHCwXbswEDRKPvVmKzAQNEo-9WYsokzAqETCFtRKYZQPWXiZvNwbRogeo6kmMWuBNSFLvY&_nc_ohc=dGYFOpKkngkAX-pdkLU&_nc_ht=scontent.fdac24-2.fna&oh=00_AT_VCBgs3yRFUy07notxgkVeeFI0fIX3j4wMUd2hoeQHzA&oe=6337109D",
   },
   {
     id: 3,
     gender: "female",
-    name: "Shakiba",
-    contact: "01925716395",
-    address: "Sherpur",
-    photoUrl: "https://www.facebook.com/moniruzzaman255/",
+    name: "Mitisha",
+    contact: "01861818986",
+    address: "Narsingdi",
+    photoUrl: "https://scontent.fdac24-1.fna.fbcdn.net/v/t1.6435-9/89196142_1229793380546952_3259142581455421440_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_eui2=AeE5iwwl3QPdR1G1DXU9MsuemzpXCFeu6aibOlcIV67pqNfmiSqNRT7Vwg5SxYl0WGxFyD18RIwj5RDVS_jutEuG&_nc_ohc=AwSAcriw3kUAX_dOU9Q&_nc_oc=AQngLHLpNcf4qU6JqMKir76a9tf8qpP-wsOuJPdJK36rXzj-rb1NoKGQsUpzQR1rXAw&_nc_ht=scontent.fdac24-1.fna&oh=00_AT-gq18rbgfztLtrmTuP7QCORWurPoyslfp4-JpeCQYXbA&oe=63397157",
   },
 ];
 
@@ -88,20 +88,6 @@ module.exports.updateMultiUser = (req, res) => {
   const { id } = req.query;
   console.log([id], [id].length);
   let newData = users.find((u) => u.id === Number(id));
-  // if (newData) {
-  //     newData = {
-  //         id: req.body.id || id,
-  //         gender: req.body.gender || newData.gender,
-  //         name: req.body.name || newData.name,
-  //         contact: req.body.contact || newData.contact,
-  //         address: req.body.address || newData.address,
-  //         photoUrl: req.body.photoUrl || newData.photoUrl
-  //     }
-  //     res.send(newData)
-  // }
-  // else {
-  //     res.send("user id doesn't exist")
-  // }
 };
 
 /* Delete a User Api */
